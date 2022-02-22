387bd68cf781d75cd3392a87d72d5239f9797938
LKaio16 <ytkkaio16@gmail.com>
2022-02-21 21:44:51 -0300

    ADICIONANDO ARQUIVOS

---
+++ src/estudo/Pessoa.java
@@ -0,0 +1,22 @@
+package estudo;
+
+public class Pessoa {
+
+	private String nome;
+	
+	private int idade;
+	
+	private String cor;
+	
+	private String cpf;
+	
+	
+	public void cadastrar(){
+	System.out.println("BEM VINDO AO SISTEMA!");	
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/estudo/principal.java
@@ -0,0 +1,13 @@
+package estudo;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		Pessoa Anderson = new Pessoa();
+		
+		Anderson.cadastrar();
+		
+	}
+
+}
