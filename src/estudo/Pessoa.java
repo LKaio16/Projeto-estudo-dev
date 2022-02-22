e1628a0eafb67731862131953c3866622882de93
LKaio16 <ytkkaio16@gmail.com>
2022-02-21 21:47:36 -0300

    ADICIONANDO ARQUIVOS

--- src/estudo/Pessoa.java
+++ src/estudo/Pessoa.java
@@ -11,6 +11,83 @@ public class Pessoa {
 	private String cpf;
 	
 	
+	
+	
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+
+
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+
+
+
+
+	public String getCor() {
+		return cor;
+	}
+
+
+
+
+
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+
+
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+
+
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+
+
+
+
 	public void cadastrar(){
 	System.out.println("BEM VINDO AO SISTEMA!");	
 		
